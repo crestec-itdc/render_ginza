c2663d1172f2e8de51f9464aea1dc4d675e5c08f
crestec-itdc <97569851+crestec-itdc@users.noreply.github.com>
2024-03-27 08:25:46 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import spacy
 
 app = FastAPI()
 
 class Item(BaseModel):
-    value: str
-    #price: float
+    name: str
+    price: float
 
     #print(BaseModel)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World2"}
 
 
 @app.get("/items/{item_id}")
@@ -23,22 +22,8 @@ def read_item(item_id: int, q: str = None):
 
 @app.post("/items")
 def update_item(item: Item):
-    print(item.value)
-
-    nlp = spacy.load('ja_ginza_electra') # or ja_ginza (in case with low memory)
-    #f = open('out.txt','r',encoding='UTF-8')
-    #src=f.read()
-    src=item.value
-
-    doc = nlp(src)
-    output = ""
-    for sent in doc.sents:
-        print(sent)
-        output = output + sent.text + "\n"
-    #f.close()
-    #return {"item_name": doc.sents}
-    #return {"output": item.name, "twice price": item.price * 2}
-    return {"output": output}
+    print(item.name)
+    return {"item_name": item.name, "twice price": item.price * 2}
 
 
 
